722cae19e987ac79ea42d09c8f8d042191bb129d
William Corotan <williamcorotan@gmail.com>
2023-02-03 10:09:22 +0800

    Initial commit

---
+++ resources/views/components/listing-tags.blade.php
@@ -0,0 +1,17 @@
+@props(['tags'])
+
+@php
+    $tags = explode(',', $tags);
+@endphp
+
+{{-- TAGS --}}
+<ul class="flex">
+
+@foreach ($tags as $tag)
+<li
+  class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-sm"
+>
+  <a href="/?tag={{ $tag }}">{{ $tag }}</a>
+</li>    
+@endforeach
+</ul>
